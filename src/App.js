0be78cc95aefdfe029a5ee937d1188738d3eb3b5
jasmineblinks <puenehfaith@gmail.com>
2022-04-08 01:31:35 +0100

    fix: updated some components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./Components/header/Header";
+
 import SideNav from "./Components/sideNav/SideNav";
+import LandingPage from "./Components/LandingPage/LandingPage";
+import Profile from "./Components/profile/Profile";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
       <SideNav />
-      <Routes></Routes>
+
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="/profile" element={<Profile />} />
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ src/Components/profile/Profile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div style={{ backgroundColor: "green", color: "#fff" }}>
+      <p style={{ color: "#fff", marginLeft: "100px" }}>Profile</p>
+    </div>
+  );
+};
+
+export default Profile;
